4a1ff496d5df555a4b379aa0ba939e35472d368c
Soroush Karimi <sorooshkarimi93@gmail.com>
2023-01-30 10:15:30 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,13 @@ const cardButton6 = document.querySelector('.card-6 button');
 cardButton6.addEventListener('click', function () {
   this.parentElement.parentElement.classList.toggle('active');
 });
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@ card-10 @@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+const cardButton10 = document.querySelector('.card-10 .card__share');
+
+cardButton10.addEventListener('click', function () {
+  this.parentElement.classList.toggle('active');
+});
